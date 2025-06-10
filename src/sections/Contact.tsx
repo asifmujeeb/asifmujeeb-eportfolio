fc1de2627a04cbe1d8bea9c40bb8d6eb6abd985a
Asif Mujeeb <57123322+asifmujeeb@users.noreply.github.com>
2025-06-10 19:57:05 -0400

    Update Contact.tsx Typo

--- src/sections/Contact.tsx
+++ src/sections/Contact.tsx
@@ -20,10 +20,10 @@ export const ContactSection = ({ id }: ContactSectionProps) => {
           <div className="flex flex-col md:flex-row gap-8 md:gap-24 items-center">
             <div>
               <h2 className="font-serif text-2xl md:text-3xl">
-                Let's create something amazing together
+                Let&rsquo;s create something amazing together
               </h2>
               <p className="text-sm md:text-base mt-2 ">
-                Ready to bring your next project to life? Let's connect and
+                Ready to bring your next project to life? Let&rsquo;s connect and
                 discuss how I can help you achieve your goals
               </p>
             </div>
