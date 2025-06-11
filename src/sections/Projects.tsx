20d027160d82e7e83d5da2968aa9f469bed02dbd
Asif Mujeeb <57123322+asifmujeeb@users.noreply.github.com>
2025-06-10 20:25:15 -0400

    Update Projects.tsx

--- src/sections/Projects.tsx
+++ src/sections/Projects.tsx
@@ -98,7 +98,7 @@ export const ProjectsSection = ({ id }: ProjectsSectionProps) => {
                   <ul className="flex flex-col gap-4 mt-4 md:mt-5">
                     
                     {project.results.map((result) => (
-                      <li className="flex gap-2 text-sm md:text-base text-white/50 ">
+                      <li key={result.title} className="flex gap-2 text-sm md:text-base text-white/50 ">
                         <CheckCircleIcon className="size-5 md:size-6" />
                         <span>{result.title}</span>
                       </li>
